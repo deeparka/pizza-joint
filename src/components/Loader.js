6857464770a7141e6b6d03251652f18934c0f408
deeparka <arkadeep180@gmail.com>
2022-07-26 13:12:03 +0530

    completed loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { motion } from "framer-motion";
+import { motion, useCycle } from "framer-motion";
 
 const loaderVariants = {
-    animatonOne: {
+    animationOne: {
         x: [-20, 20],
         y: [0, -30],
         transition: {
@@ -10,16 +10,27 @@ const loaderVariants = {
             y: { yoyo: Infinity, duration: 0.25, ease: "easeOut" },
         },
     },
+    animationTwo: {
+        y: [0, -40],
+        x: 0,
+        transition: { y: { yoyo: Infinity, duration: 0.25, ease: "easeOut" } },
+    },
 };
 
 const Loader = () => {
+    const [animation, cycleAnimation] = useCycle(
+        "animationOne",
+        "animationTwo"
+    );
+
     return (
         <>
             <motion.div
                 className="loader"
                 variants={loaderVariants}
-                animate="animatonOne"
+                animate={animation}
             ></motion.div>
+            <div onClick={() => cycleAnimation()}>Cycle Loader</div>
         </>
     );
 };
